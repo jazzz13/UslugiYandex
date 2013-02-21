183d7743e831674186d00133eb43e8a9671b08ee
jazzz13 <murtazin13@gmail.com>
2013-02-21 16:06:18 +0600

    php party part 1

--- index.php
+++ index.php
@@ -1,1 +1,57 @@
-<?php phpInfo(); ?>
+<?php 
+	
+	$formStructureCollection = array(
+		array(
+			"sum",
+			"period",
+			"purpose",
+			"region",
+			"bank",
+			"proof-of-income",
+			"advanced-repayment"
+			),
+		array(
+			"a",
+			"h"
+			),
+		array(),
+	);
+
+	$formCreditId = 0;
+	
+	if($_GET["formCreditId"]) {
+		$formCreditId = (int)($_GET["formCreditId"]);
+	}
+
+
+	$selectedFields = $formStructureCollection[$formCreditId];
+
+	function getDescriptionByFieldName($fieldName){
+		return $fieldName." lol";
+	}
+
+	function getControlsByFildName($fieldName){
+		return "lol ".$fieldName;
+	}
+
+?>
+
+<link rel='stylesheet' type='text/css' href='./uy-style.css'>
+<div id='uslugi-yandex'>
+	<div id='uy-form'>
+		<table id='uy-form-table'>
+
+<?php
+	foreach ($selectedFields as $fieldName) {
+		
+		echo "<tr><td>";
+		echo getDescriptionByFieldName( $fieldName );	
+		echo "</td><td>";
+		echo getControlsByFildName( $fieldName );
+		echo "</td></tr>";	
+	}	
+?>
+
+		</table>
+	</div>
+</div>
